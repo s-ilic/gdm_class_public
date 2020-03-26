d0220bee4090279fba48ebb41a65c44e01fec255
Stephane Ilic <stephane.ilic@gmail.com>
2020-03-26 20:42:38 +0100

    Changed name of python wrapper from 'classy' to 'gdm_classy'

--- python/setup.py
+++ python/setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(include_folder, 'common.h'), 'r') as v_file:
             break
 
 # Define cython extension and fix Python version
-classy_ext = Extension("classy", [os.path.join(classy_folder, "classy.pyx")],
+classy_ext = Extension("gdm_classy", [os.path.join(classy_folder, "classy.pyx")],
                            include_dirs=[nm.get_include(), include_folder],
                            libraries=liblist,
                            library_dirs=[root_folder, GCCPATH],
@@ -43,7 +43,7 @@ import six
 classy_ext.cython_directives = {'language_level': "3" if six.PY3 else "2"}
         
 setup(
-    name='classy',
+    name='gdm_classy',
     version=VERSION,
     description='Python interface to the Cosmological Boltzmann code CLASS',
     url='http://www.class-code.net',
