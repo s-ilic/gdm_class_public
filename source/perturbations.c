94288cec44fa2a594c2924f8741e2ffbb64090c3
Stephane Ilic <stephane.ilic@gmail.com>
2020-03-28 22:20:35 +0100

    Typo corrected

--- source/perturbations.c
+++ source/perturbations.c
@@ -5194,14 +5194,14 @@ int perturb_initial_conditions(struct precision * ppr,
 
     /* GDM_CLASS
     /* this has implications for how the Einstein equations are written using the
-       radiatio/matter ratio rho_m_over_rho_r. The fld is supposed to be close to
+       radiation/matter ratio rho_m_over_rho_r. The fld is supposed to be close to
        cdm regarding the background: |w| << 1 */
     if (pba->has_gdm == _TRUE_) {
       /* This next test ensures that integration starts early enough for the
          simplified GDM initial conditions to be valid */
-      class_test(ppr->start_small_k_at_tau_c_over_tau_h <= 1e-6,
+      class_test(ppr->start_small_k_at_tau_c_over_tau_h > 1e-6,
                  ppt->error_message,
-                 "The precision parameter 'start_small_k_at_tau_c_over_tau_h' is too low (= %e) for GDM, use a value <= 1e-6.");
+                 "The precision parameter 'start_small_k_at_tau_c_over_tau_h' is too high (= %e) for GDM, use a value <= 1e-6.");
       rho_m += ppw->pvecback[pba->index_bg_rho_gdm];
     }
     /* END GDM_CLASS */
